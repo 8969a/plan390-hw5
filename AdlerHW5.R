f4f564a726dfaa5a606ab0d85ecdfe6adcc90057
Em Adler <mha25@duke.edu>
2022-04-19 12:21:33 -0400

    Created first graph, added a couple words, re-created graph. Shortened number of topics.

--- AdlerHW5.R
+++ AdlerHW5.R
@@ -7,11 +7,17 @@ speeches=read.csv("presidential_speeches_sample.csv")
 speeches$content <- recode(speeches$content,"health care"="healthcare")
 tokenized = unnest_tokens(speeches, word, content)
 custom_stop_words = bind_rows(
-  stop_words, tibble(word=c("president","jennings","people","lot","country",
+  stop_words, tibble(word=c("president","jennings","people","lot","question","bush","minister"
   ))
 )
 tokenized = anti_join(tokenized, custom_stop_words, by="word")
 tokenized = mutate(tokenized, lemma=lemmatize_words(word))
 word_counts = group_by(tokenized, document, lemma) %>% summarize(count=n())
-word_matrix = cast_dtm(wcounts, document, lemma, count)
-model = LDA(word_matrix, 35, control=list(seed=18))
+word_matrix = cast_dtm(word_counts, document, lemma, count)
+speech_topic_model = LDA(word_matrix, 24, control=list(seed=18))
+beta_matrix = tidy(speech_topic_model,matrix="beta")
+topics=group_by(beta_matrix, topic) %>% slice_max(beta, n=10)
+ggplot(topics, aes(y=reorder_within(term, beta, topic), x=beta)) +
+  geom_col() +
+  facet_wrap(~topic, scales="free") +
+  scale_y_reordered()
